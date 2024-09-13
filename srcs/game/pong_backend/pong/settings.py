9ee45ecbb5c7a1358828873c70537b7767c0dbbc
Hamza Barrak <hamzabarrak83@gmail.com>
2024-09-14 00:55:47 +0100

    enabled debugging

--- srcs/game/pong_backend/pong/settings.py
+++ srcs/game/pong_backend/pong/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=c(myl0(zz!oe1n==ukv3#lt=u*fr%d0wb2n&pt&-it$%3m4ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
