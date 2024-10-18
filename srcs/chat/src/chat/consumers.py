53529655ebfd1a715fae1fbee1629cb800591c44
werrahma <errahmaniwalid15@gmail.com>
2024-10-18 11:43:02 +0100

    fix cancel 'text content for play button' problem when decline event happen

--- srcs/chat/src/chat/consumers.py
+++ srcs/chat/src/chat/consumers.py
@@ -8,15 +8,16 @@ from .views import get_messages, get_user_contact, get_current_ChatID, get_parti
 class NotificationConsumer(WebsocketConsumer):
     def GetParticipants(self, data):
         # print(f"too is ::: {data['to']}")
-        print('----------------------------', flush=True)
-        print(data, flush=True)
+        # print('----------------------------', flush=True)
+        # print(data, flush=True)
         if data['block'] == 'false' and data['playwith'] == 'null':
             content = {
                 'message': data['message'],
                 'to': data['to'],
                 'flag': data['flag'],
                 'img': data['img'],
-                'from': data['from']
+                'from': data['from'],
+                'block': 'false'
             }
         elif data['block'] == 'false' and data['playwith'] != 'null':
             content = {
