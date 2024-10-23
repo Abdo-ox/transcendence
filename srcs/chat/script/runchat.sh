3d359a8811dc1938b8913ad56359762f23f8b152
werrahma <errahmaniwalid15@gmail.com>
2024-10-23 20:56:16 +0100

    commit to get last commit from main branch

--- srcs/chat/script/runchat.sh
+++ srcs/chat/script/runchat.sh
@@ -25,5 +25,4 @@ EOF
 
 python manage.py migrate
 
-service redis-server start
 python manage.py runserver 0.0.0.0:8000
--- srcs/game/script/script.sh
+++ srcs/game/script/script.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sleep 10
 
 hypercorn game/pong.asgi:application --bind 0.0.0.0:8000 --certfile certs/crt.crt --keyfile certs/crt.key
