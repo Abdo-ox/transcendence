83d0b6d50e5b6bbdcb7790f1b4dab2efbbd88aa1
abdo-ox <abderrahim.jari6@gmail.com>
2024-10-26 15:58:32 +0100

    working on the page scroll

--- srcs/db_nginx/front-end/script/utils.js
+++ srcs/db_nginx/front-end/script/utils.js
@@ -90,20 +90,27 @@ export const getJWT = async () => {
     return is_valid(access, refresh);
 }
 function addErrorDiv() {
-    document.body.innerHTML += `
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-            <path
-                d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-        </symbol>
-    </svg>
-    <div class="error-div alert alert-danger d-flex align-items-center" id="error-div" role="alert">
-        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
-            <use xlink:href="#exclamation-triangle-fill" />
+    if (document.getElementById('error-container')){
+        console.log("hello===================================================================================>", document.getElementById('error-container'));
+        return ;
+
+    }
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                <path
+                    d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+            </symbol>
         </svg>
-        <div class="d-flex flex-column" id="error-container">
-        </div>
-    </div>`;
+        <div class="error-div alert alert-danger d-flex align-items-center" id="error-div" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+                <use xlink:href="#exclamation-triangle-fill" />
+            </svg>
+            <div class="d-flex flex-column" id="error-container">
+            </div>
+        </div>`;
+    document.body.appendChild(div);
 }
 
 export function printErrorInScreen(errors) {
@@ -146,8 +153,8 @@ export const NewPage = async (url, func, addhistory = true,query='') => {
         }
         if (addhistory)
             history.pushState({}, '', url + query);
-        // addErrorDiv();
-        func();
+        await func();
+        addErrorDiv();
     } else
         console.log("error in fetch the new page '", url, "'.");
 }
@@ -212,10 +219,8 @@ export const makePageActive = (page) => {
         document.getElementById(key).removeEventListener('click', func[key]);
     //add event listener for non active pages
     for (const key in func) {
-        if (!key.includes(page)){
-            console.log("add event listener", document.getElementById(key));
+        if (!key.includes(page))
             document.getElementById(key).addEventListener('click', func[key]);
-        }
     }
     //the default styling for all header li a
     document.querySelectorAll('.header-li-a-style').forEach(element => {
