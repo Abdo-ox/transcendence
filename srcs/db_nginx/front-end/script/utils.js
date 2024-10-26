dff0f4ff4312f76b99b94f83c0901a827fb6647f
abdo-ox <abderrahim.jari6@gmail.com>
2024-10-26 11:05:24 +0100

    remove the duplication in addError and prontErrorInScreen functions

--- srcs/db_nginx/front-end/script/utils.js
+++ srcs/db_nginx/front-end/script/utils.js
@@ -116,33 +116,6 @@ export function printErrorInScreen(errors) {
     } ,1500);
 }
 
-function addErrorDiv() {
-    document.body.innerHTML += `
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-            <path
-                d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-        </symbol>
-    </svg>
-    <div class="error-div alert alert-danger d-flex align-items-center" id="error-div" role="alert">
-        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
-            <use xlink:href="#exclamation-triangle-fill" />
-        </svg>
-        <div class="d-flex flex-column" id="error-container">
-        </div>
-    </div>`;
-}
-
-export function printErrorInScreen(errors) {
-    const diverror = document.getElementById("error-container");
-    diverror.innerHTML = '';
-    diverror.parentElement.classList.add('show');
-    errors.forEach(error => diverror.innerHTML+= `<p>${error}</p>`);
-    setTimeout(() => {
-        diverror.parentElement.classList.remove('show');
-    } ,1500);
-}
-
 export const NewPage = async (url, func, addhistory = true,query='') => {
     console.log(" new page called for the url ", url ,query);
     const response = await fetch(url + ".html"+query);
@@ -169,11 +142,8 @@ export const NewPage = async (url, func, addhistory = true,query='') => {
         }
         if (document.querySelector('header'))
             makePageActive(url.substring(1));
-<<<<<<< HEAD
         func();
         addErrorDiv();
-=======
->>>>>>> 9bfdebad8d4832a195f15068d16de2c44f652c33
         if (addhistory)
             history.pushState({}, '', url+query);
     } else
